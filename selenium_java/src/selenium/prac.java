c04108f0ac4f30e31cf508aaef2ec98d476fcad1
anild <anildhumal333@gmail.com>
2022-04-06 14:42:29 +0530

    pushing selenium java

---
+++ selenium_java/src/selenium/prac.java
@@ -0,0 +1,18 @@
+package selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class prac {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.setProperty("webdriver.chrome.driver","C:\\chromre driver\\chromedriver_win32 (2)//chromedriver.exe");
+	     
+	     WebDriver driver=new ChromeDriver();
+	     driver.get("https://www.youtube.com/watch?v=xi7oibukBJE&list=PLUDwpEzHYYLs6G9WOfFxiH5zMfgNxgmSV&index=12");
+		
+		
+	}
+
+}
